18fe89f01790edd3a82d859cef62c23d0311189c
Matthew <matthew96923@gmail.com>
2018-04-28 16:53:17 +0800

    parallax scrolling

---
+++ Travel/js/hidemenu.js
@@ -0,0 +1,14 @@
+function hideMenu() {
+	var prevScrollpos = window.pageYOffset;
+	window.onscroll = function() {
+		var currentScrollPos = window.pageYOffset;
+		if (prevScrollpos - currentScrollPos >= 0) {
+			document.getElementById("navbar").style.top = "0";
+		} else {
+			document.getElementById("navbar").style.top = "-150px";
+		}
+		prevScrollpos = currentScrollPos;
+	}
+}
+
+hideMenu();
