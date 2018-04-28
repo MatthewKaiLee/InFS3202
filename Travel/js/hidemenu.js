68a845acdf189ab0e5a0d40fe818bec81502eb21
Matthew <matthew96923@gmail.com>
2018-04-28 17:07:35 +0800

    change back

--- Travel/js/hidemenu.js
+++ Travel/js/hidemenu.js
@@ -2,7 +2,7 @@ function hideMenu() {
 	var prevScrollpos = window.pageYOffset;
 	window.onscroll = function() {
 		var currentScrollPos = window.pageYOffset;
-		if (prevScrollpos - currentScrollPos >= 0) {
+		if (prevScrollpos <= currentScrollPos) {
 			document.getElementById("navbar").style.top = "0";
 		} else {
 			document.getElementById("navbar").style.top = "-150px";
