56f273ba43f15de39db5e9ccde0f30e3fc664db8
Jiajie Li <kakit_lee@hotmail.com>
2018-05-11 16:55:09 +1000

    update

--- Travel/connectdatabase.php
+++ Travel/connectdatabase.php
@@ -1,7 +1,7 @@
 <?php 
 	header("Content-Type: text/html; charset=utf-8");
 	$db_username = "root";
-	$db_password = "1234";
+	$db_password = "";
 	$db_link = @mysqli_connect("localhost", $db_username, $db_password);
 	if (!$db_link) {
 		die("connect database fail");
